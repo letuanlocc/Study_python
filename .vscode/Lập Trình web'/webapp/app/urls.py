225baccc136388edc186212bdcd242614e1f9cab
letuanlocc <locletuan5@gmail.com>
2025-03-06 23:14:52 +0700

    Delete .vscode/Lập Trình web' directory

--- .vscode/Lập Trình web'/webapp/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.home),
-    path('link/',views.link_view, name ="link_page"),
-    path('menu/',views.menu_view,name ="menu_page"),
-    path('milk/',views.milk_view, name = "milk_page"),
-]
