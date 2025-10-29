ebf48994a4214047556c77e4ac55a292c8e88f35
Le Tuan Loc <locletuan5@gmail.com>
2025-10-29 20:20:31 +0700

    Remove note_list folder from main repo tracking

--- .vscode/note_list/note/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from rest_framework.routers import DefaultRouter
-from .views import NoteViewSet
-from django.contrib import admin
-router = DefaultRouter()
-router.register(r'notes', NoteViewSet, basename='notes')
-urlpatterns = [
-    path('', include(router.urls)),
-]
